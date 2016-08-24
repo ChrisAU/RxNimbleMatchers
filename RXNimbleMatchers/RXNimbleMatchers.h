97a6b77da9687540ecfff38892b9103571be4dfa
Dennis Schmidt <dmrschmidt@gmail.com>
2016-08-24 20:58:39 +0200

    add project

---
+++ RXNimbleMatchers/RXNimbleMatchers.h
@@ -0,0 +1,19 @@
+//
+//  RXNimbleMatchers.h
+//  RXNimbleMatchers
+//
+//  Created by Dennis Schmidt on 24.08.16.
+//  Copyright © 2016 Dennis Schmidt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RXNimbleMatchers.
+FOUNDATION_EXPORT double RXNimbleMatchersVersionNumber;
+
+//! Project version string for RXNimbleMatchers.
+FOUNDATION_EXPORT const unsigned char RXNimbleMatchersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RXNimbleMatchers/PublicHeader.h>
+
+
